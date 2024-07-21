0ba4b87d88e200914ed9520fad4cbd27848a0754
insshubh <insshubh22@gmail.com>
2024-07-21 16:51:24 +0530

    latest

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,13 +6,13 @@ import { BsX } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-    <a target="_blank" href="https://www.linkedin.com/in/insshubh/">
+    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/insshubh/">
       <BsLinkedin style={{ fontSize: "1.5rem" }} /> 
     </a>
-    <a target="_blank" href="https://github.com/insshubh">
+    <a target="_blank" rel="noreferrer" href="https://github.com/insshubh">
       <FaGithub style={{ fontSize: "1.5rem" }} /> 
     </a>
-    <a target="_blank" href="https://x.com/insshubh">
+    <a target="_blank" rel="noreferrer" href="https://x.com/insshubh">
       <BsX style={{ fontSize: "1.5rem" }} /> 
     </a>
   </div>
